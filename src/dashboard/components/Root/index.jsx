8393a654501c1b9c4aa2ff433873a825f6f0b055
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2016-12-29 16:45:27 +0100

    Done html design

--- src/dashboard/components/Root/index.jsx
+++ src/dashboard/components/Root/index.jsx
@@ -2,16 +2,104 @@ import React from 'react';
 import * as deps from '../../deps';
 
 const Content = () => (
-  <div>
-    <h1>I am a Worona package</h1>
-    <div>And this is my content.</div>
+  <div className="columns is-multiline">
+    <div className="column is-5 is-12-mobile is-half-tablet">
+      <div className="card is-fullwidth">
+        <div className="card-content">
+          <div className="columns is-mobile">
+            <div className="column is-3">
+              <img src="http://placehold.it/90x90" alt="Push Notifications icon" />
+            </div>
+            <div className="column is-9">
+              <div className="content">
+                <h2>Push Notifications</h2>
+                <p>
+                  WordPress out of the box is already technically quite a good platform for SEO, this was true when Joost wrote his original WordPress SEO article in 20 …
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <footer className="card-footer">
+          <span className=" card-footer-item"><strong><small>Coming soon</small></strong></span>
+        </footer>
+      </div>
+    </div>
+    <div className="column is-5 is-12-mobile is-half-tablet">
+      <div className="card is-fullwidth">
+        <div className="card-content">
+          <div className="columns is-mobile">
+            <div className="column is-3">
+              <img src="http://placehold.it/90x90" alt="Ads icon" />
+            </div>
+            <div className="column is-9">
+              <div className="content">
+                <h2>Ads</h2>
+                <p>
+                  WordPress out of the box is already technically quite a good platform for SEO, this was true when Joost wrote his original WordPress SEO article in 20 …
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <footer className="card-footer">
+          <span className=" card-footer-item"><strong><small>Coming soon</small></strong></span>
+        </footer>
+      </div>
+    </div>
+    <div className="column is-5 is-12-mobile is-half-tablet">
+      <div className="card is-fullwidth">
+        <div className="card-content">
+          <div className="columns is-mobile">
+            <div className="column is-3">
+              <img src="http://placehold.it/90x90" alt="Analytics icon" />
+            </div>
+            <div className="column is-9">
+              <div className="content">
+                <h2>Analytics</h2>
+                <p>
+                  WordPress out of the box is already technically quite a good platform for SEO, this was true when Joost wrote his original WordPress SEO article in 20 …
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <footer className="card-footer">
+          <span className=" card-footer-item"><strong><small>Coming soon</small></strong></span>
+        </footer>
+      </div>
+    </div>
+    <div className="column is-5 is-12-mobile is-half-tablet">
+      <div className="card is-fullwidth">
+        <div className="card-content">
+          <div className="columns is-mobile">
+            <div className="column is-3">
+              <img src="http://placehold.it/90x90" alt="Rate my App icon" />
+            </div>
+            <div className="column is-9">
+              <div className="content">
+                <h2>Rate my App</h2>
+                <p>
+                  WordPress out of the box is already technically quite a good platform for SEO, this was true when Joost wrote his original WordPress SEO article in 20 …
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <footer className="card-footer">
+          <span className=" card-footer-item"><strong><small>Coming soon</small></strong></span>
+        </footer>
+      </div>
+    </div>
   </div>
 );
 
 export default () => {
   const RootContainer = deps.elements.RootContainer;
   return (
-    <RootContainer mobilePreview>
+    <RootContainer>
+      <h1>Extensions</h1>
+      <hr />
       <Content />
     </RootContainer>
   );
